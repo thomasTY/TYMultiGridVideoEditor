fa042de3ca3bc04c4e271c322d784e4159326797
xiangnan <xorlory@outlook.com>
2025-06-25 03:34:46 +0800

    移除无用文件

--- Sources/Editor/ComponentViews.swift
+++
@@ -1,4 +0,0 @@
-                .aspectRatio(canvasAspectRatio, contentMode: .fit)
-                .padding(.horizontal, 12)
-                // .padding(.top, 8) // 移除顶部padding，保证顶部对齐
-                .padding(.bottom, 0) 

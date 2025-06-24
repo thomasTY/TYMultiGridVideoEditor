145e71bb1d646ebcf928e15743d07f77eae6fbf1
xiangnan <xorlory@outlook.com>
2025-06-24 21:42:55 +0800

    画布区

---
+++ Sources/Editor/ComponentViews.swift
@@ -0,0 +1,4 @@
+                .aspectRatio(canvasAspectRatio, contentMode: .fit)
+                .padding(.horizontal, 12)
+                // .padding(.top, 8) // 移除顶部padding，保证顶部对齐
+                .padding(.bottom, 0) 
